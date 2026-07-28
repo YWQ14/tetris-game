e0f5e41e9a7e0759697c96b57ed682bf863620f0
YWQ14 <wang.yingi@gmail.com>
2026-07-28 15:20:01 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/tetris-game/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
